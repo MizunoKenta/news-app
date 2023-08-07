ab6c85ed0452cfb5e0cbb69c368538e5f9aa750a
mizuno.kenta <mizuno.kenta@tis.co.jp>
2023-08-07 17:51:24 +0900

    api疎通

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { ListItem } from "./components/ListItem";
-import dummyArticles from "./dummies/articles";
 import { useState, useEffect } from "react";
+import axios from "axios";
+
+// newsapi.orgのAPIからニュース記事を取得。API_KEYは公開しないようダミーで仮置き。
+// 最終的にはExpoのConstantsに記載したい。
+const URL = "https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=API_KEY";
 
 export default function App() {
   const { articles, setArticles } = useState({});
 
-  const fetchArticles = () => {
-    setArticles(dummyArticles);
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      // 確認用の出力
+      console.log(response.data.articles);
+      setArticles(response.data.articles);
+    } catch {
+      console.error(error);
+    }
   };
 
+  // 画面初期化時に1度だけ実行する処理
   useEffect(() => {
     fetchArticles();
-  });
-  // const items = articles.map((article, index) => {
-  //   return (
-  //     <ListItem
-  //       ImageUrl={article.urlToImage}
-  //       title={article.title}
-  //       author={article.author}
-  //       key={index.toString()}
-  //     />
-  //   );
-  // });
+  }, {});
 
   return (
     <SafeAreaView style={styles.container}>
